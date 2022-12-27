b7301448b102fb5b0bac5378bd1967d60ecc2c84
Sarwarhridoy4 <sarwarhridoy4@gmail.com>
2022-12-27 14:52:58 +0600

    Coming Soon Added

---
+++ src/components/Coming Soon/ComingSoon.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./ComingSoon.css";
+const ComingSoon = () => {
+  return (
+    <div className='loader w-full h-screen bg-white dark:bg-slate-900'>
+      <div className='scanner'>
+        <span className="inline-block my-[30vh]">Coming Soon...</span>
+      </div>
+    </div>
+  );
+};
+
+export default ComingSoon;
--- src/components/Router/Routes/Routes.jsx
+++ src/components/Router/Routes/Routes.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../../Layout/Main/Main";
+import ComingSoon from "../../Coming Soon/ComingSoon";
 import Error from "../../Error/Error";
 import Home from "../../Home/Home";
 
@@ -12,7 +13,19 @@ const router = createBrowserRouter([
             {
                 path: "/",
                 element: <Home></Home>,
-        }
+            },
+            {
+                path: "/add-a-task",
+                element:<ComingSoon></ComingSoon>
+            },
+            {
+                path: "/my-tasks",
+                element:<ComingSoon></ComingSoon>
+            },
+            {
+                path: "/completed-task",
+                element:<ComingSoon></ComingSoon>
+            },
     ]
     }
   ]);
