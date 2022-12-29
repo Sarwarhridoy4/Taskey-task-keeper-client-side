539c704e211817de84f1308891eaf2fa6abb580a
Sarwarhridoy4 <sarwarhridoy4@gmail.com>
2022-12-29 14:44:26 +0600

    log in signup done

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,16 +1,66 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { GoogleAuthProvider } from 'firebase/auth';
+import React, { useContext, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { AuthContext } from '../../contexts/Auth/AuthContext';
 
 const Login = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+  // const onSubmit = data => console.log(data);
+  const { signIn, googleSignIn } = useContext(AuthContext);
+  const googleProvider = new GoogleAuthProvider();
+  const [loginError, setloginError] = useState('')
+  const locaton = useLocation()
+  const navigate = useNavigate()
+    const from = locaton.state?.from?.pathname || '/';
+    
+    //login
+    const handelLogIn = (data) => {
+        // console.log(data);
+        setloginError('');
+        signIn(data.email, data.password)
+          .then(result => {
+            const user = result.user
+            toast.success('User login Successfully!')
+            console.log(user); 
+            navigate(from,{replace:true})
+          })
+          .catch(error => {
+            console.error(error)
+            setloginError(error.message)
+          })
+    }
+    
+    //google signin
+  const handelGoogleSignIn = () => {
+    googleSignIn(googleProvider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        toast(`authenticated as ${user?.displayName}`)
+        navigate("/");
+       
+      })
+      .catch((error) => {
+        console.error(error.message);
+        toast(error.message)
+      });
+  };
+
     return (
         <div className='bg-white dark:bg-slate-700 py-5'>
             <div class="w-full max-w-sm p-6 m-auto mx-auto bg-white rounded-lg shadow-md dark:bg-gray-800">
     <h1 class="text-3xl font-semibold text-center text-gray-700 dark:text-white">Taskey</h1>
 
-    <form class="mt-6">
+    <form onSubmit={handleSubmit(handelLogIn)} class="mt-6">
         <div>
-            <label for="username" class="block text-sm text-gray-800 dark:text-gray-200">Email</label>
-            <input type="email" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
+            <label for="email" class="block text-sm text-gray-800 dark:text-gray-200">Email</label>
+                        <input type="email"
+                            placeholder='Email'
+                            {...register("email",{ required: "Provide Email" })}
+                            class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
+                        {errors.email && <p className="text-red-500 block" role="alert">{errors.email?.message}</p>}
         </div>
 
         <div class="mt-4">
@@ -19,7 +69,13 @@ const Login = () => {
                 <a href="#" class="text-xs text-gray-600 dark:text-gray-400 hover:underline">Forget Password?</a>
             </div>
 
-            <input type="password" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
+                        <input type="password"
+                            placeholder='password'
+                            {...register("password", {
+                                required: "Password is required",
+                              })}
+                            class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
+                        {errors.password && <p className="text-red-500 block" role="alert">{errors.password?.message}</p>}
         </div>
 
         <div class="mt-6">
@@ -40,7 +96,7 @@ const Login = () => {
     </div>
 
     <div class="flex items-center mt-6 -mx-2">
-        <button type="button" class="flex items-center justify-center w-full px-6 py-2 mx-2 text-sm font-medium text-white transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:bg-blue-400 focus:outline-none">
+        <button onClick={handelGoogleSignIn} type="button" class="flex items-center justify-center w-full px-6 py-2 mx-2 text-sm font-medium text-white transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:bg-blue-400 focus:outline-none">
             <svg class="w-4 h-4 mx-2 fill-current" viewBox="0 0 24 24">
                 <path d="M12.24 10.285V14.4h6.806c-.275 1.765-2.056 5.174-6.806 5.174-4.095 0-7.439-3.389-7.439-7.574s3.345-7.574 7.439-7.574c2.33 0 3.891.989 4.785 1.849l3.254-3.138C18.189 1.186 15.479 0 12.24 0c-6.635 0-12 5.365-12 12s5.365 12 12 12c6.926 0 11.52-4.869 11.52-11.726 0-.788-.085-1.39-.189-1.989H12.24z">
                 </path>
@@ -57,8 +113,13 @@ const Login = () => {
         </a>
     </div>
 
-    <p class="mt-8 text-xs font-light text-center text-gray-400"> Don't have an account? <Link to ='/signup' class="font-medium text-gray-700 dark:text-gray-200 hover:underline">Create One</Link></p>
-</div>
+    <p class="mt-8 text-xs font-light text-center text-gray-400"> Don't have an account? <Link to='/signup' class="font-medium text-gray-700 dark:text-gray-200 hover:underline">Create One</Link></p>
+            </div>
+            {
+                
+                    loginError && toast.error(loginError)
+                  
+            }
         </div>
     );
 };
